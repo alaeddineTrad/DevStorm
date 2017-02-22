b56266163ed37260a5a4e1d80460217a7553dacb
alaeddineTrad <mohamedalaeddine.trad@esprit.tn>
2017-02-22 23:05:23 +0100

    Entity+MVC+productModel

---
+++ Domain/Entities/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Domain.Entities
+{
+    public class Review
+    {
+
+        public virtual Showroomer Showroomer { get; set; }
+        public virtual Buyer Buyer { get; set; }
+
+        public Review()
+        {
+
+        }
+    }
+}
